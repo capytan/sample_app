34f6399056291389e1d3f54a74abcbd6800711e2
Yosuke Mizuguchi <mizuguchi.yosuke.fcp@gmail.com>
2018-05-19 04:12:09 +0000

    finish 10.3.4

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,12 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:index, :edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
   # GET /users/:id
+  
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+  
   def show
     @user = User.first
     @user = User.find(params[:id])
@@ -46,4 +53,21 @@ class UsersController < ApplicationController
       :name, :email, :password,
       :password_confirmation)
   end
+  
+  # beforeアクション
+
+    # ログイン済みユーザーかどうか確認
+    def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
+
+    # 正しいユーザーかどうか確認
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
 end
